ad2217a0c8bec7cbb3a585e89c407aadb035fb4c
Andrea Righi <andrea.righi@wishraiser.com>
2022-12-01 08:47:16 +0100

    initial commit

---
+++ utility/file_reader.py
@@ -0,0 +1,6 @@
+def read_file(file_path, optional_divider=None):
+    f = open(file_path, "r")
+    if optional_divider is not None:
+        return f.read().split(optional_divider)
+    else:
+        return f.read().split()
